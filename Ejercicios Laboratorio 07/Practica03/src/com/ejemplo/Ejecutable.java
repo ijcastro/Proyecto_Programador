d8ac2b46a57ff974b9a3be329f9ae53e7cfd5fd2
ijcastro <ignaciojocastro@gmail.com>
2020-09-19 09:54:03 -0300

    Resolucion Practica03 Lab07

---
+++ Ejercicios Laboratorio 07/Practica03/src/com/ejemplo/Ejecutable.java
@@ -0,0 +1,22 @@
+package com.ejemplo;
+
+import com.ejemplo.gui.JFConversor;
+
+/**
+ *
+ * @author Nacho
+ */
+public class Ejecutable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFConversor conversor = new JFConversor();
+        conversor.setVisible(true);
+        
+        
+        
+    }
+    
+}
